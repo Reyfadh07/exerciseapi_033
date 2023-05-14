376764ba1719ac2bad053095a5ecf6f0cbe4a78a
Reyfadh07 <raihan.farmasi@gmail.com>
2023-05-14 18:12:28 +0700

    menambahkan delete kategori barang pada kategori barang dart di view ,button delete dll

--- lib/view/kategori_barang.dart
+++ lib/view/kategori_barang.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:materiapi/controller/kategori_barang_controller.dart';
 import 'package:materiapi/model/kategori_barang_model.dart';
 import 'package:materiapi/view/kategori_barang.dart';
+import 'package:materiapi/view/update_kategori_barang.dart';
 
 import '../model/kategori_barang_model.dart';
 import 'add_kategori_barang.dart';
@@ -32,7 +33,8 @@ class _KategoriBarangState extends State<KategoriBarang> {
       listKategoriBarang = kategoriBarang;
     });
   }
-  
+  void deleteKategoriBarang(int id) async {
+    await kategoriBarangController.deleteKategoriBarang(id);
   
   
 
@@ -53,15 +55,23 @@ class _KategoriBarangState extends State<KategoriBarang> {
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
                   IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  const UpdateKategoriBarang()));
+                    },
                     icon: const Icon(Icons.edit),
                   ),
                   IconButton(
-                    onPressed: () {
-                      //deleteKategoriBarang();
-                    },
-                    icon: const Icon(Icons.delete),
-                  )
+                      onPressed: () {
+                        deleteKategoriBarang(listKategoriBarang[index].id);
+                        setState(() {
+                          listKategoriBarang.removeAt(index);
+                        });
+                      },
+                      icon: const Icon(Icons.delete))
                 ],
               ),
             ),
