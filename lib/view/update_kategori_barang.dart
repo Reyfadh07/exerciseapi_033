92c551965a6318e63045b5e07dcf80892f871fd1
Reyfadh07 <raihan.farmasi@gmail.com>
2023-05-14 13:11:20 +0700

    menambahkan isi update kategori barang

--- lib/view/update_kategori_barang.dart
+++ lib/view/update_kategori_barang.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -11,7 +12,40 @@ class UpdateKategoriBarang extends StatefulWidget {
 class _UpdateKategoriBarangState extends State<UpdateKategoriBarang> {
   @override
   Widget build(BuildContext context) {
-    
-    return const Placeholder();
+    var formkey = GlobalKey<FormState>();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Kategori Barang'),
+      ),
+      body: Form(
+        key: formkey,
+        child: ListView(
+          padding: const EdgeInsets.all(17.0),
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Nama Kategori Barang',
+                hintText: 'Nama Kategori Barang',
+              ),
+              onChanged: (value) {},
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Nama Kategori diperlukan';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                if (formkey.currentState!.validate()) {}
+              },
+              child: const Text('simpan'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
